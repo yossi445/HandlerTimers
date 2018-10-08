c754675caa8cd7090f9db23b24bd47823987e6c0
yossi <yossi445@gmail.com>
2018-10-08 09:20:00 +0300

    two timers

--- app/src/main/java/com/example/yossi/handlertimers/MainActivity.java
+++ app/src/main/java/com/example/yossi/handlertimers/MainActivity.java
@@ -10,12 +10,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    TextView tv1;
-    Button btnPlus1, btnMin1;
+    TextView tv1,tv2;
+    Button btnPlus1, btnMin1, btnPlus2, btnMin2;
     int start = 0;
-    Handler handler;
+    Handler handler, handler2;
 
-    ThreadTimer timer1;
+    ThreadTimer timer1,timer2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +26,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnPlus1 = findViewById(R.id.btnPlus1);
         btnMin1 = findViewById(R.id.btnMin1);
 
+        tv2 = findViewById(R.id.tv2);
+        btnPlus2 = findViewById(R.id.btnPlus2);
+        btnMin2 = findViewById(R.id.btnMin2);
+
         btnPlus1.setOnClickListener(this);
         btnMin1.setOnClickListener(this);
+
+        btnPlus2.setOnClickListener(this);
+        btnMin2.setOnClickListener(this);
 
 
 
@@ -37,13 +44,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 tv1.setText(String.valueOf(msg.arg1));
 
+
+
+                return true;
+            }
+        });
+
+        handler2 = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+
+                tv2.setText(String.valueOf(msg.arg1));
+
+
                 return true;
             }
         });
 
         timer1 = new ThreadTimer(handler);
         timer1.start();
-
+        timer2 = new ThreadTimer(handler2);
+        timer2.start();
 
     }
 
@@ -53,8 +74,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             timer1.speedUp();
         else if(v == btnMin1)
             timer1.speedDown();
+        else if(v == btnPlus2)
+            timer2.speedUp();
+        else if(v == btnMin2)
+            timer2.speedDown();
 
-        //...
+
 
     }
 }
